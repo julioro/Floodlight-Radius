c663fe5b30d2ca9e78d5c94fec873c4a61fbd7f9
julioro <julio.rodriguezo@pucp.pe>
2019-06-27 14:37:22 -0500

    Se crea ambas clases de paquete, creo que solo uno sirve.

---
+++ src/main/java/net/floodlightcontroller/packet/EAP_OVER_LAN.java
@@ -0,0 +1,17 @@
+package net.floodlightcontroller.packet;
+
+public class EAP_OVER_LAN extends BasePacket{
+
+	@Override
+	public byte[] serialize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPacket deserialize(byte[] data, int offset, int length) throws PacketParsingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/net/floodlightcontroller/packet/RADIUS.java
@@ -0,0 +1,17 @@
+package net.floodlightcontroller.packet;
+
+public class RADIUS extends BasePacket{
+
+	@Override
+	public byte[] serialize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPacket deserialize(byte[] data, int offset, int length) throws PacketParsingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
