1d65f6047c2d94411f05baa414d3200e7e3ffb57
julioro <julio.rodriguezo@pucp.pe>
2019-07-15 16:22:08 -0500

    Intento con StaticFlowPusher y FlowMods.

--- src/main/java/net/floodlightcontroller/radius/Radius.java
+++ src/main/java/net/floodlightcontroller/radius/Radius.java
@@ -10,10 +10,19 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import org.projectfloodlight.openflow.protocol.OFFlowMod;
 import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.OFVersion;
 import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.types.DatapathId;
 import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.OFBufferId;
+import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.U64;
 
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IOFMessageListener;
@@ -22,8 +31,12 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.util.AppCookie;
+import net.floodlightcontroller.forwarding.Forwarding;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.staticflowentry.IStaticFlowEntryPusherService;
+import net.floodlightcontroller.staticflowentry.StaticFlowEntryPusher;
+
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.Set;
@@ -35,11 +48,11 @@ import net.floodlightcontroller.packet.RADIUS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Radius implements IFloodlightModule, IOFMessageListener {
+public class Radius extends Forwarding implements IFloodlightModule, IOFMessageListener {
 
 	protected IFloodlightProviderService floodlightProvider;
 	protected static Logger logger;
-	//protected IStaticFlowEntryPusherService sfp;
+	protected IStaticFlowEntryPusherService sfp;
 
 	@Override
 	public String getName() {
@@ -63,43 +76,37 @@ public class Radius implements IFloodlightModule, IOFMessageListener {
 	@Override
 	public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
 			FloodlightContext cntx) {
-		logger.info("\n\n\nSE ESTÁ EJECUTANDO EL MODULO:\n\tRADIUS\n\n\n");
-		
-		/*
-		 * ... COMPLETAR REVISAR BIEN QUE DEBE IR AQUI ACA SE DEFINE EL COMPORTAMIENTO
-		 * DEL MODULO ...
-		 */
-		
 		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-		logger.info("Ethertype:\t"+eth.getEtherType().toString());
-		logger.info(eth.toString());
-		
+		logger.info("\nRadius.java\n");
 		switch (msg.getType()) {
 		case PACKET_IN:
 			if (eth.getEtherType() == EthType.EAP_OVER_LAN) {
+
 				logger.info(eth.toString());
 				logger.info("OFMessage:\t" + msg.toString());
 				logger.info("PACEKTS IN ETHERTYPE EAP\n\n\n\n\n");
-				
-				List<OFAction> actionList = new ArrayList<>();
-	        		actionList.add(sw.getOFFactory().actions().output(OFPP_LOCAL_INT, Integer.MAX_VALUE));
-	        		OFPacketOut.Builder po = sw.getOFFactory().buildPacketOut()
-	                		/*.setData(data) No enviamos data. */
-	                		.setActions(actionList)
-	                		.setInPort(OFPort.CONTROLLER)
-	                		.setBufferId(OFBufferId.NO_BUFFER);
-	        		///Enviamos el PacketOut
-	        		try {
-						messageDamper.write(sw, po.build());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-				}
 
+				OFPacketIn pi = (OFPacketIn) msg;
+				OFPort inPort = (pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort()
+						: pi.getMatch().get(MatchField.IN_PORT));
+				Match m = createMatchFromPacket(sw, inPort, cntx);
+				OFFlowMod.Builder fmb = sw.getOFFactory().buildFlowAdd();
+				List<OFAction> actions = new ArrayList<OFAction>(); // set no action to drop
+				actions.add(sw.getOFFactory().actions().output(OFPort.LOCAL, Integer.MAX_VALUE));
 
+				U64 cookie = AppCookie.makeCookie(FORWARDING_APP_ID, 0);
+				fmb.setCookie(cookie).setHardTimeout(FLOWMOD_DEFAULT_HARD_TIMEOUT)
+						.setIdleTimeout(FLOWMOD_DEFAULT_IDLE_TIMEOUT).setBufferId(OFBufferId.NO_BUFFER).setMatch(m)
+						.setPriority(FLOWMOD_DEFAULT_PRIORITY).setActions(actions);
+
+				OFFlowMod fm = fmb.build();
+				DatapathId swDpid = (DatapathId) sw.getId();
+				String name = "G";
+
+				sfp.addFlow(name, fm, swDpid);
 
 			}
-			
+
 			break;
 		default:
 			logger.info("NUNCA DEBE EJECUTARSE ESTO.\n\n\n\n\n");
